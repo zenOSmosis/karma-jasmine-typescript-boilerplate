7a5c36b56ded00999fbcf52415873ce73ae22621
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-11-05 23:17:10 -0500

    Remove duplicate test

--- src/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import mockUtility, { MockEnum } from "../src/index";
-
-describe("mock utility", () => {
-  it("returns expected value", () => {
-    expect(mockUtility()).toBe(MockEnum.MockVal1);
-  });
-});
